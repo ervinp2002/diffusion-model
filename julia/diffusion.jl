adc523d83212ed32a6bb6467483290461c6ec180
Ervin E. Pangilinan <Ervin.Escalona.Pangilinan@live.mercer.edu>
2022-11-05 16:01:16 -0400

    Starting Julia

---
+++ julia/diffusion.jl
@@ -0,0 +1,51 @@
+#=
+Ervin Pangilinan
+CSC 330: Organization of Programming Languages - Fall 2022
+Project 2: Simplified 3D Diffusion Model
+Implementation in Julia
+=#
+
+maxSize = isa(ARGS[1], Number) ? Int64(ARGS[1]) : 10
+
+
+println(maxSize)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
