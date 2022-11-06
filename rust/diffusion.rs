2c49876ac932e9cd31c0d7d27a44a6e5d89cf04c
Ervin E. Pangilinan <Ervin.Escalona.Pangilinan@live.mercer.edu>
2022-11-06 16:39:10 -0500

    Starting Rust

---
+++ rust/diffusion.rs
@@ -0,0 +1,17 @@
+/*
+Ervin Pangilinan
+CSC 330: Organization of Programming Languages - Fall 2022
+Project 2: Simplified 3D Diffusion Model
+Implementation in Rust
+*/
+
+use std::env;
+
+fn main() {
+    // PRE: Command line arguments are passed to determine subdivisions and partition in room. 
+    // POST: Outputs the ratio of concentration and time taken to equilibrate the room. 
+
+    let args : Vec<String> = env::args().collect();     // Contains command-line flags to be parsed. 
+    
+
+}
