01e2ab9e69e8ef80a4ad6144e65e754bb813bbed
Ervin E. Pangilinan <Ervin.Escalona.Pangilinan@live.mercer.edu>
2022-11-18 02:07:30 -0500

    Implemented partition

--- ada/diffusion.adb
+++ ada/diffusion.adb
@@ -3,8 +3,8 @@
 -- Project 2: Simplified 3D Diffusion Model
 -- Implementation in Ada
 
-with Ada.Text_IO, Ada.Float_Text_IO, Ada.Command_Line, Ada.Characters.Latin_1;
-use Ada.Text_IO, Ada.Float_Text_IO, Ada.Command_Line, Ada.Characters.Latin_1;
+with Ada.Text_IO, Ada.Integer_Text_IO, Ada.Float_Text_IO, Ada.Command_Line, Ada.Characters.Latin_1;
+use Ada.Text_IO, Ada.Integer_Text_IO ,Ada.Float_Text_IO, Ada.Command_Line, Ada.Characters.Latin_1;
 
 procedure Diffusion is
 -- MAIN PROGRAM
@@ -22,16 +22,16 @@ begin
 
     declare 
         -- Predefined Variables
-        diffusionCoefficient: Long_Float := 0.175;
-        roomDimension: Long_Float := 5.0;
-        speedOfGasMolecules: Long_Float := 250.0;
-        timestep: Long_Float := (roomDimension / speedOfGasMolecules) / Long_Float(maxSize);
+        diffusionCoefficient: Long_Float := 0.175;      
+        roomDimension: Long_Float := 5.0;                                                       -- In meters.
+        speedOfGasMolecules: Long_Float := 250.0;                                               -- Based on 100 g/mol. 
+        timestep: Long_Float := (roomDimension / speedOfGasMolecules) / Long_Float(maxSize);    -- h in seconds.
         distanceBetweenBlocks: Long_Float := roomDimension / Long_Float(maxSize);
         DTerm: Long_Float := (diffusionCoefficient * timestep) / (distanceBetweenBlocks * distanceBetweenBlocks);
 
         -- Variables to be modified during simulation
         room: Cube(1..maxSize, 1..maxSize, 1..maxSize);
-        mask: BitMask(1..maxSize + 2, 1..maxSize + 2, 1..maxSize + 2);
+        mask: BitMask(1..maxSize, 1..maxSize, 1..maxSize);
         change: Long_Float;
         sumval: Long_Float;
         minval: Long_Float;
@@ -44,60 +44,21 @@ begin
             mask := (others => (others => (others => 0)));
 
             -- Set up the 75% partition. 
-            for j in Integer(Float'Ceiling(Float(maxSize / 4) - 1.0))..maxSize + 2 loop 
-                for k in 1..maxSize + 2 loop 
-                    mask(Integer(Float'Ceiling(Float(maxSize / 2))), j, k) := 1;
-                end loop;
-            end loop;
-
-            -- Set up the front-facing wall. 
-            for j in 1..maxSize + 2 loop 
-                for k in 1..maxSize + 2 loop 
-                    mask(1, j, k) := 1;
-                end loop;
-            end loop;
-
-            -- Set up the back-facing wall. 
-            for j in 1..maxSize + 2 loop 
-                for k in 1..maxSize + 2 loop 
-                    mask(maxSize + 2, j, k) := 1;
-                end loop;
-            end loop;
-
-            -- Set up the ceiling. 
-            for i in 1..maxSize + 2 loop 
-                for k in 1..maxSize + 2 loop 
-                    mask(i, 1, k) := 1;
-                end loop;
-            end loop;
-
-            -- Set up the floor. 
-            for i in 1..maxSize + 2 loop 
-                for k in 1..maxSize + 2 loop 
-                    mask(i, maxSize + 2, k) := 1;
-                end loop;
-            end loop;
-
-            -- Set up the left-facing wall. 
-            for i in 1..maxSize + 2 loop 
-                for j in 1..maxSize + 2 loop 
-                    mask(i, j, 1) := 1;
-                end loop;
-            end loop;
-
-            -- Set up the right-facing wall. 
-            for i in 1..maxSize + 2 loop 
-                for j in 1..maxSize + 2 loop 
-                    mask(i, j, maxSize + 2) := 1;
+            for j in Integer(Float'Truncation(float((maxSize / 4) + 1)))..maxSize loop
+                for k in 1..maxSize loop 
+                    mask(Integer(Float'Truncation(float(maxSize / 2))), j, k) := 1;
                 end loop;
             end loop;
         end if;
 
+        -- Initialize the room to 0.
         room := (others => (others => (others => 0.0)));
         time := 0.0;
         change := 0.0;
         sumval := 0.0;
         ratio := 0.0;
+
+        -- Set the first point in the room to hold all of the molecules.
         room(1, 1, 1) := 1.0e21;
 
         while ratio < 0.99 loop
@@ -114,7 +75,7 @@ begin
                                     or (i = l + 1 and j = m and k = n)          -- Move forward
                                     or (i = l - 1 and j = m and k = n) then     -- Move backward
                                         if partitionPresent then
-                                            if mask(i + 1, j + 1, k + 1) = 0 and mask(l + 1, m + 1, n + 1) = 0 then
+                                            if mask(i, j, k) = 0 and mask(l, m, n) = 0 then
                                                 change := Long_Float((room(i, j, k) - room(l, m, n)) * DTerm);
                                                 room(i, j, k) := room(i, j, k) - change;
                                                 room(l, m, n) := room(l, m, n) + change;
@@ -139,6 +100,7 @@ begin
             for i in 1..maxSize loop
                 for j in 1..maxSize loop
                     for k in 1..maxSize loop
+                        -- Prevents accessing a zeroed index because of the partition. 
                         if room(i, j, k) /= 0.0 then
                             minval := Long_Float'Min(room(i, j, k), minval);
                             maxval := Long_Float'Max(room(i, j, k), maxval);
