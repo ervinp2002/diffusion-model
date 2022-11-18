0269a3a3c73fa2f8e0fff9fd6d069baa25c23f4e
Ervin E. Pangilinan <Ervin.Escalona.Pangilinan@live.mercer.edu>
2022-11-18 00:44:37 -0500

    Partition needs testing

--- ada/diffusion.adb
+++ ada/diffusion.adb
@@ -11,8 +11,9 @@ procedure Diffusion is
 -- PRE: Command line argument is passed to specfiy amount of subdivisions. 
 -- POST: Outputs simulated time with specified data points.
     type Cube is array (Integer range <>, Integer range <>, Integer range <>) of Long_Float;
-    type Mask is array (Integer range<>, Integer range <>, Integer range<>) of Integer;
+    type BitMask is array (Integer range<>, Integer range <>, Integer range<>) of Integer;
     maxSize: Integer := Integer'Value(Argument(1));
+    partitionPresent: Boolean := (if Argument_Count >= 2 and Argument(Argument_Count - 0) = "partition" then True else False);
     
 begin
     if maxSize < 0 then
@@ -30,6 +31,7 @@ begin
 
         -- Variables to be modified during simulation
         room: Cube(1..maxSize, 1..maxSize, 1..maxSize);
+        mask: BitMask(1..maxSize + 2, 1..maxSize + 2, 1..maxSize + 2);
         change: Long_Float;
         sumval: Long_Float;
         minval: Long_Float;
@@ -38,6 +40,59 @@ begin
         time: Long_Float;
 
     begin
+        if partitionPresent then
+            mask := (others => (others => (others => 0)));
+
+            -- Set up the 75% partition. 
+            for j in Integer(Float'Ceiling(Float(maxSize / 4) - 1.0))..maxSize + 2 loop 
+                for k in 1..maxSize + 2 loop 
+                    mask(Integer(Float'Ceiling(Float(maxSize / 2))), j, k) := 1;
+                end loop;
+            end loop;
+
+            -- Set up the front-facing wall. 
+            for j in 1..maxSize + 2 loop 
+                for k in 1..maxSize + 2 loop 
+                    mask(1, j, k) := 1;
+                end loop;
+            end loop;
+
+            -- Set up the back-facing wall. 
+            for j in 1..maxSize + 2 loop 
+                for k in 1..maxSize + 2 loop 
+                    mask(maxSize + 2, j, k) := 1;
+                end loop;
+            end loop;
+
+            -- Set up the ceiling. 
+            for i in 1..maxSize + 2 loop 
+                for k in 1..maxSize + 2 loop 
+                    mask(i, 1, k) := 1;
+                end loop;
+            end loop;
+
+            -- Set up the floor. 
+            for i in 1..maxSize + 2 loop 
+                for k in 1..maxSize + 2 loop 
+                    mask(i, maxSize + 2, k) := 1;
+                end loop;
+            end loop;
+
+            -- Set up the left-facing wall. 
+            for i in 1..maxSize + 2 loop 
+                for j in 1..maxSize + 2 loop 
+                    mask(i, j, 1) := 1;
+                end loop;
+            end loop;
+
+            -- Set up the right-facing wall. 
+            for i in 1..maxSize + 2 loop 
+                for j in 1..maxSize + 2 loop 
+                    mask(i, j, maxSize + 2) := 1;
+                end loop;
+            end loop;
+        end if;
+
         room := (others => (others => (others => 0.0)));
         time := 0.0;
         change := 0.0;
@@ -58,9 +113,17 @@ begin
                                     or (i = l and j = m - 1 and k = n)          -- Move left
                                     or (i = l + 1 and j = m and k = n)          -- Move forward
                                     or (i = l - 1 and j = m and k = n) then     -- Move backward
-                                        change := Long_Float((room(i, j, k) - room(l, m, n)) * DTerm);
-                                        room(i, j, k) := Long_Float(room(i, j, k) - change);
-                                        room(l, m, n) := Long_Float(room(l, m, n) + change);
+                                        if partitionPresent then
+                                            if mask(i + 1, j + 1, k + 1) = 0 and mask(l + 1, m + 1, n + 1) = 0 then
+                                                change := Long_Float((room(i, j, k) - room(l, m, n)) * DTerm);
+                                                room(i, j, k) := room(i, j, k) - change;
+                                                room(l, m, n) := room(l, m, n) + change;
+                                            end if;
+                                        else 
+                                            change := Long_Float((room(i, j, k) - room(l, m, n)) * DTerm);
+                                            room(i, j, k) := room(i, j, k) - change;
+                                            room(l, m, n) := room(l, m, n) + change;
+                                        end if;
                                     end if;
                                 end loop;
                             end loop;
@@ -76,9 +139,11 @@ begin
             for i in 1..maxSize loop
                 for j in 1..maxSize loop
                     for k in 1..maxSize loop
-                        minval := (if room(i, j, k) < minval  then room(i, j, k) else minval);
-                        maxval := (if room(i, j, k) > maxval  then room(i, j, k) else maxval);
-                        sumval := sumval + room(i, j, k);
+                        if room(i, j, k) /= 0.0 then
+                            minval := Long_Float'Min(room(i, j, k), minval);
+                            maxval := Long_Float'Max(room(i, j, k), maxval);
+                            sumval := sumval + room(i, j, k);
+                        end if;
                     end loop;
                 end loop;
             end loop;
